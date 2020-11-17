406d03e6f99a1edd1c1fba857c14e6c1b6920ffb
Jared Huff <jhuff21@biomedscienceacademy.org>
2020-11-17 04:52:38 +0000

    final draft

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ MadLibs
 Author: Jared Huff
 Period/Core:5
 input()Enter a name: Mark
-adj = input()Enter a adjective:Fat
+adj = input()Enter a adjective:good
 adj2 = input()Enter a adjective:nice
 food = input()Enter a food:tacos
 food2 = input()Enter a food:pizza 
-noun = input()Enter a noun:Georgia
+noun = input()Enter a noun:Mcdonalds
 place = input()Enter a place:Atlanta
 verb = input()Enter a verb:running
 #part two of process
-print("[Garrison] was planning a dream vacation to [North Korea].")
-print("[Marcey] was planning a dream vacation to [Germany].")
+print("{name} wanted to visit his family in {place}.")
+print("{name} wanted some {food2} through {noun}.")
+
+print("{name} loves to go fishing in {place}.")
+
+print("{name} needed a haircut but {noun} doesnt have clippers.")
+print("{name} thinks {adj} is a synonyms to {adj2}.")
+
+print("{noun} is not {adj} for the body but {verb} is.")
