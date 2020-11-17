2bacc0712ea6717876316ca229f218981f911806
Jared Huff <jhuff21@biomedscienceacademy.org>
2020-11-17 04:28:43 +0000

    checking

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-"""
+print("lets play silly sentences!")
 MadLibs
-Author: 
-Period/Core:
-
-
-"""
+Author: Jared Huff
+Period/Core:5
+input()Enter a name: Mark
+adj = input()Enter a adjective:Fat
+adj2 = input()Enter a adjective:nice
+food = input()Enter a food:tacos
+food2 = input()Enter a food:pizza 
+noun = input()Enter a noun:Georgia
+place = input()Enter a place:Atlanta
+verb = input()Enter a verb:running
+#part two of process
+print("[Garrison] was planning a dream vacation to [North Korea].")
+print("[Marcey] was planning a dream vacation to [Germany].")
